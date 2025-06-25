ae30797121d6c435fe1acaca04f7eb25f492dfcc
Deva Kumar (AIML) <117890712+devasphn@users.noreply.github.com>
2025-06-25 06:30:33 +0530

    Update download_models.py

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -2,7 +2,7 @@ from huggingface_hub import snapshot_download
 
 # Download the latest Ultravox v0.5 1B model
 snapshot_download(
-    repo_id="fixie-ai/ultravox-v0_5-llama-3_2-1b",  # Correct repository ID
+    repo_id="fixie-ai/ultravox-v0_5-llama-3_2-8b",  # Correct repository ID
     local_dir="models/ultravox",
     repo_type="model",
     revision="main",
